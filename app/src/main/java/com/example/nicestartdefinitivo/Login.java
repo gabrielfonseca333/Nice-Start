1ddbd696092c4f324e91387c0c05cd38752773b3
gabrielfonseca333 <gabrielfonsecag@gmail.com>
2023-11-20 16:03:31 +0100

    I add the code of the Login.java of the procedures "openMain" and "openSignup"

--- app/src/main/java/com/example/nicestartdefinitivo/Login.java
+++ app/src/main/java/com/example/nicestartdefinitivo/Login.java
@@ -2,7 +2,9 @@ package com.example.nicestartdefinitivo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Login extends AppCompatActivity {
 
@@ -11,4 +13,19 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
+    //Método que se va a usar al pulsar en el Button LOGIN
+    public void openMain(View v){
+        Intent intent = new Intent(Login.this, Main.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+    }
+
+    //Método que se va a usar al pulsar en el Button SINGUP
+    public void openSignup(View v) {
+        Intent intent = new Intent(Login.this, Signup.class);
+        startActivity(intent);
+    }
+
 }
--- app/src/main/java/com/example/nicestartdefinitivo/Signup.java
+++ app/src/main/java/com/example/nicestartdefinitivo/Signup.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Singup extends AppCompatActivity {
+public class Signup extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
