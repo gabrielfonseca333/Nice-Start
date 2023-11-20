14c12a85746054eeb955fcd6c5ac317fe337d047
gabrielfonseca333 <gabrielfonsecag@gmail.com>
2023-11-20 13:53:29 +0100

    Initial commit

---
+++ app/src/main/java/com/example/nicestartdefinitivo/Singup.java
@@ -0,0 +1,14 @@
+package com.example.nicestartdefinitivo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Singup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_singup);
+    }
+}
