0b8341bfdb0c23ebf4f096fb8b9b74df58669132
gabrielfonseca333 <gabrielfonsecag@gmail.com>
2023-11-22 20:52:43 +0100

    I add the .xml and .java Splash of my app.

--- app/src/main/java/com/example/nicestartdefinitivo/Splash.java
+++ app/src/main/java/com/example/nicestartdefinitivo/Splash.java
@@ -2,13 +2,49 @@ package com.example.nicestartdefinitivo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 
 public class Splash extends AppCompatActivity {
+
+    ImageView imFoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        imFoto = findViewById(R.id.backView);
+
+        Glide.with(this)
+                .load("https://images.pexels.com/photos/2213575/pexels-photo-2213575.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")
+                .transition(DrawableTransitionOptions.withCrossFade(100))
+                .centerCrop()
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                //.placeholder(new ColorDrawable(this.getResources().getColor(R.color.teal_200)))
+                .into(imFoto);
     }
+
+    private void openAp(){
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash.this, Login.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+
+            }
+        },5000);
+
+    }
+
 }
