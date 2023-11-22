1d5efe46a8023e5c1c34a664b8b5adee31f5ee7a
gabrielfonseca333 <gabrielfonsecag@gmail.com>
2023-11-22 13:41:41 +0100

    I create the code to use the button in the Singup.java

--- app/src/main/java/com/example/nicestartdefinitivo/Signup.java
+++ app/src/main/java/com/example/nicestartdefinitivo/Signup.java
@@ -2,7 +2,9 @@ package com.example.nicestartdefinitivo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Signup extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class Signup extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_singup);
+
     }
+
+    //metodo a ejecutar el Cancel
+    public void openLogin(View v){
+
+        Intent intent = new Intent(Signup.this, Login.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+
+    }
+
 }
